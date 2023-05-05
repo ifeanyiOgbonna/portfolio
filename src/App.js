e55e43def93245f9fcd54d9fadb7871ec499449f
logicalOgbonna <arinnzeogbo@gmail.com>
2023-05-05 02:01:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <a href="#home" className='logo'>Divine.</a>
+
+        <nav className='navbar'>
+          <a href="#home" className='active'>Home</a>
+          <a href="#about">About</a>
+          <a href='#service'>Service</a>
+          <a href='#portfolio'>Portfolio</a>
+          <a href="#contact">Contact</a>
+        </nav>
       </header>
-    </div>
+
+      <section className='home'>
+        <div className='home-content'>
+          <h1>Hi, I'm Ifeanyi Ogbonna</h1>
+          <h3>Frontend Developer</h3>
+          <p>"I'm a frontend developer, which means I specialize in creating the visual and interactive elements of websites or applications. I work with HTML, CSS, and JavaScript to build user interfaces that are engaging, intuitive, and responsive."</p>
+          <div className='btn-box'>
+            <a href="#">Hire Me</a>
+            <a href="#">Let's Talk</a>
+          </div>
+        </div>
+        <div className='home-sci'>
+        <a href="https://www.linkedin.com/feed/update/urn:li:activity:7058184034132123648/"><i class='bx bxl-linkedin'></i></a>
+        <a href="https://twitter.com/IfeanyiIfybest/status/1652052332715077633"><i class='bx bxl-twitter' ></i></a>
+        <a href="#"><i class='bx bxl-medium' ></i></a>
+        </div>
+        
+      </section>
+    </>
   );
 }
 
 export default App;
+
